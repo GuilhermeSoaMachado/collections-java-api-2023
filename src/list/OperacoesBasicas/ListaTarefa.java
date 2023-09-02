6b162884438aa50451baaf5a0da75b1bca1448a4
GuilhermeMachado <develid.io@gmail.com>
2023-09-01 22:17:56 -0300

    atualizaç

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,29 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+       List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+               tarefasParaRemover.add(t);
+            }
+
+        }
+            
+    }
+
+}
